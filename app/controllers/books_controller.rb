fba0c1dcc944c1f3aa5fcb19a1d7be4f2814568b
Flynn <flynnwebdev@gmail.com>
2022-04-27 12:02:03 +1000

    Completed CRUD controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,37 @@
 class BooksController < ApplicationController
   # DEBUG - REMOVE FOR PRODUCTION!!!
   skip_before_action :verify_authenticity_token
+  before_action :set_book, except: [:index, :create] #only: [:show, :update, :destroy]
 
   def index
     @books = Book.all
   end
 
   def show
-    begin
-    @book = Book.find(params[:id])
-    rescue
-      render plain: "Book not found!"
-    end
   end
 
   def create
-    render plain: 'books#create'
+    book = Book.create(book_params)
+    redirect_to book
+  end
+
+  def update
+    @book.update(book_params)
+    redirect_to @book
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    return params.permit(:title, :author_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'books', to: 'books#index'
   get 'books/:id', to: 'books#show', as: 'book'
+  put 'books/:id', to: 'books#update' 
   post 'books', to: 'books#create'
+  delete 'books/:id', to: 'books#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
