34a5d93b60c19826bee981a6dd2f6abd5dbd4b13
Flynn <flynnwebdev@gmail.com>
2022-05-03 12:27:01 +1000

    Added Devise; Fixed flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get 'users', to: 'devise/sessions#new'
+  end
+
+  devise_for :users
   get 'books', to: 'books#index'
   get 'books/new', to: 'books#new'
   get 'books/:id', to: 'books#show', as: 'book'
   get 'books/:id/edit', to: 'books#edit', as: 'edit_book'
-  patch 'books/:id', to: 'books#update' 
+  patch 'books/:id', to: 'books#update'
   post 'books', to: 'books#create'
   delete 'books/:id', to: 'books#destroy'
 
